39a9174ce2a6fbaa4cdf9807ba3bdf504757aed4
dazacher <dazacher2000@yahoo.com>
2021-07-16 07:42:02 -0700

    Playing with mod and hex numbers.

--- src/numops.adb
+++ src/numops.adb
@@ -1,6 +1,21 @@
+with Ada.Text_IO;
+
 procedure Numops is
 
+    package TIO renames Ada.Text_IO;
+    package IIO is new TIO.Integer_IO(Integer);
+    type Mod_Byte is mod 256;
+    package MIO is new TIO.Modular_IO(Mod_Byte);
+
+    Test_Mod : Mod_Byte := 16#FF#;
 begin
-   --  Insert code here.
-   null;
+    TIO.Put_Line("Modular Test ");
+    MIO.Put(Test_Mod);
+    Test_Mod := Test_Mod and 7;
+    MIO.Put(Test_Mod);
+    MIO.Put(Test_Mod + 255);
+    MIO.Put(Test_Mod or 16#FF#);
+    MIO.Put(Test_Mod * 16);
+    TIO.New_Line;
+
 end Numops;
